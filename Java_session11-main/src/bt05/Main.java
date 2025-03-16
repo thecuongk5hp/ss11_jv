7dc23673171e998c5816ee69c543382bb7b7f521
thecuongk5hp <147390984+thecuongk5hp@users.noreply.github.com>
2025-03-16 22:09:56 +0700

    Update Main.java

--- Java_session11-main/src/bt05/Main.java
+++ Java_session11-main/src/bt05/Main.java
@@ -2,15 +2,15 @@ package bt05;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(5.0);
-        Rectangle rectangle = new Rectangle(4.0, 6.0);
-        Square square = new Square(3.0);
+        Circle circle = new Circle(3.0);
+        Rectangle rectangle = new Rectangle(2.0, 3.0);
+        Square square = new Square(5.0);
 
-        circle.setColor("Xanh");
+        circle.setColor("Trấng");
         rectangle.setColor("Đỏ");
-        square.setColor("Vàng");
+        square.setColor("Đen");
 
-        System.out.println("\n=== Kiểm tra màu sắc ===");
+        System.out.println("\n========== Kiểm tra màu sắc ===========");
         System.out.println("Màu của hình tròn: " + circle.getColor());
         System.out.println("Màu của hình chữ nhật: " + rectangle.getColor());
         System.out.println("Màu của hình vuông: " + square.getColor());
